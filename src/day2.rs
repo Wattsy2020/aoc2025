4d51826e0b68a15002b3475aae6403da9abdd1c6
Liam Watts <liam.watts271@gmail.com>
2026-01-01 20:08:55 +1100

    day2: part1 refactor to make part2 easier

--- src/day2.rs
+++ src/day2.rs
@@ -30,7 +30,23 @@ fn parse_input(input: &str) -> Vec<Range> {
 /// Return a number with the digit at place value `decimal_place` in `num` set to `digit`
 /// Assumes the digit was zero previously, and that `digit` is in [0, 9]
 fn set_digit(num: i64, decimal_place: u32, digit: u8) -> i64 {
+    assert!(digit <= 9);
     num + (digit as i64 * 10i64.pow(decimal_place))
+}
+
+/// Return a number with the digits at place value `decimal_place` - `n`*`repetition_distance` in `num` set to `digit`
+/// Assumes the digit was zero previously, that `digit` is in [0, 9]
+fn set_digit_repeating(num: i64, decimal_place: u32, repetition_distance: u32, digit: u8) -> i64 {
+    let mut result = num;
+    let mut current_place = decimal_place;
+    loop {
+        result = set_digit(result, current_place, digit);
+        if current_place < repetition_distance {
+            break;
+        }
+        current_place -= repetition_distance;
+    }
+    result
 }
 
 /// Return the number of digits in `num`
@@ -55,9 +71,8 @@ fn find_invalid_ids_from_number(
     current_num: i64,
     current_decimal_place: u32,
     start_decimal_place: u32,
+    repetition_distance: u32,
 ) -> Vec<i64> {
-    let repetition_distance = start_decimal_place - (start_decimal_place / 2);
-
     // check if we reached the second repetition,
     // in which case this number is already repeated twice and thus is an invalid id
     if current_decimal_place + repetition_distance == start_decimal_place {
@@ -71,9 +86,10 @@ fn find_invalid_ids_from_number(
     // try all possible numbers for this first digit
     (0..10)
         .map(|digit| {
-            set_digit(
-                set_digit(current_num, current_decimal_place, digit), // set the current place
-                current_decimal_place - repetition_distance, // then also set the digit that needs to repeat this digit
+            set_digit_repeating(
+                current_num,
+                current_decimal_place,
+                repetition_distance,
                 digit,
             )
         })
@@ -83,21 +99,24 @@ fn find_invalid_ids_from_number(
             // if the number is less than the min, even if the remaining places were set to 99999..., then it can't be a solution
             && num + 10i64.pow(current_decimal_place) - 1 >= range.start)
         .flat_map(|num| {
-            find_invalid_ids_from_number(range, num, current_decimal_place - 1, start_decimal_place)
+            find_invalid_ids_from_number(
+                range,
+                num,
+                current_decimal_place - 1,
+                start_decimal_place,
+                repetition_distance,
+            )
         })
         .collect()
 }
 
-fn find_invalid_ids(range: &Range) -> Vec<i64> {
-    // find decimal place of start and end digit
-    // split up the search to within multiple ranges that each have the same decimal place
-    // (skip odd numbered decimal places)
-    // this simplifies the problem for `find_invalid_ids_from_number`,
-    // it only has to consider numbers with the same number of decimal places
+/// Split up the range to multiple ranges that each have the same decimal place.
+/// This simplifies the problem for `find_invalid_ids_from_number`,
+/// it only has to consider numbers with the same number of decimal places
+fn get_decimal_place_aligned_ranges(range: &Range) -> Vec<Range> {
     let num_start_digits = num_digits(range.start);
     let num_end_digits = num_digits(range.end);
-    let ranges: Vec<Range> = (num_start_digits..=num_end_digits)
-        .filter(|num_digits| *num_digits > 0u32 && num_digits % 2 == 0)
+    (num_start_digits..=num_end_digits)
         .map(|num_digits| Range {
             start: if num_digits == num_start_digits {
                 range.start
@@ -110,13 +129,27 @@ fn find_invalid_ids(range: &Range) -> Vec<i64> {
                 10i64.pow(num_digits) - 1
             },
         })
-        .collect();
+        .collect()
+}
 
-    ranges
-        .iter()
+fn find_invalid_ids(range: &Range) -> Vec<i64> {
+    get_decimal_place_aligned_ranges(range)
+        .into_iter()
         .flat_map(|range| {
-            let start_decimal_place = num_digits(range.start) - 1;
-            find_invalid_ids_from_number(&range, 0, start_decimal_place, start_decimal_place)
+            let num_digits = num_digits(range.start);
+            if num_digits == 0u32 || num_digits % 2 != 0 {
+                return vec![];
+            }
+
+            let start_decimal_place = num_digits - 1;
+            let repetition_distance = num_digits / 2; // for part1 repeat numbers twice
+            find_invalid_ids_from_number(
+                &range,
+                0,
+                start_decimal_place,
+                start_decimal_place,
+                repetition_distance,
+            )
         })
         .collect()
 }
@@ -124,6 +157,9 @@ fn find_invalid_ids(range: &Range) -> Vec<i64> {
 pub fn solve_part1(input: &str) -> i64 {
     parse_input(input).iter().flat_map(find_invalid_ids).sum()
 }
+
+// part2: same general principle but we have to generalise find_invalid_ids
+// to take a `num_repetitions` parameter, then call it with all possible repetitions for that number of decimal places
 
 #[cfg(test)]
 mod tests {
@@ -147,9 +183,19 @@ mod tests {
     }
 
     #[test]
+    fn test_set_digit_repeating() {
+        assert_eq!(set_digit_repeating(0, 3, 1, 3), 3333);
+        assert_eq!(set_digit_repeating(0, 3, 2, 3), 3030);
+        assert_eq!(set_digit_repeating(0, 3, 3, 3), 3003);
+        assert_eq!(set_digit_repeating(0, 3, 4, 3), 3000);
+        assert_eq!(set_digit_repeating(0, 3, 5, 3), 3000);
+        assert_eq!(set_digit_repeating(0, 7, 3, 5), 50050050);
+    }
+
+    #[test]
     fn test_find_invalid_ids_for_number() {
         assert_eq!(
-            find_invalid_ids_from_number(&Range { start: 10, end: 99 }, 0, 1, 1),
+            find_invalid_ids_from_number(&Range { start: 10, end: 99 }, 0, 1, 1, 1),
             vec![11, 22, 33, 44, 55, 66, 77, 88, 99]
         );
         assert_eq!(
@@ -160,7 +206,8 @@ mod tests {
                 },
                 0,
                 3,
-                3
+                3,
+                2
             ),
             vec![1010, 1111, 1212]
         );
